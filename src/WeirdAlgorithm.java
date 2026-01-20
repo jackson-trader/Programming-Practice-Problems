ebbfdfbba470611620304b88421f895adce70038
jackson-trader <jackson.trader.dev@gmail.com>
2026-01-20 16:24:07 -0600

    Commit

---
+++ src/WeirdAlgorithm.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class WeirdAlgorithm {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        while (n != 1) {
+            // Even number
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                n = (n * 3) + 1;
+            }
+            System.out.print(n + " ");
+        }
+
+    }
+}
